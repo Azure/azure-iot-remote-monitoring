ecbb10279ebb9d5f22e632594ee4ca895f7c1ceb
Keith Craig <keithc@vertigo.com>
2015-10-08 13:48:11 -0700

    Minor code cleanup

--- DeviceAdministration/Web/Controllers/DashboardController.cs
+++ DeviceAdministration/Web/Controllers/DashboardController.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Mvc;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.DeviceSchema;
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.DeviceSchema;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Exceptions;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Models;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Models;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
+using StringPair = System.Collections.Generic.KeyValuePair<string, string>;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Controllers
 {
-    using StringPair = KeyValuePair<string, string>;
-    using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common;
-    using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
-
     /// <summary>
     /// A Controller for Dashboard-related views.
     /// </summary>
@@ -23,22 +21,12 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
     [OutputCache(CacheProfile = "NoCacheProfile")]
     public class DashboardController : Controller
     {
-        #region Constants
-
         private const double MaxDeviceSummaryAgeMinutes = 10.0;
         private const int MaxDevicesToDisplayOnDashboard = 200;
-
-        #endregion
-
-        #region Instance Variables
 
         private readonly IDeviceLogic _deviceLogic;
         private readonly IDeviceTelemetryLogic _deviceTelemetryLogic;
         private readonly IConfigurationProvider _configProvider;
-
-        #endregion
-
-        #region Constructors
 
         /// <summary>
         /// Initializes a new instance of the DashboardController class.
@@ -74,12 +62,6 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
             _deviceTelemetryLogic = deviceTelemetryLogic;
             _configProvider = configProvider;
         }
-
-        #endregion
-
-        #region Public Methods
-
-        #region Instance Method: Index
 
         [RequirePermission(Permission.ViewTelemetry)]
         public async Task<ActionResult> Index()
@@ -130,10 +112,6 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
 
             return View(model);
         }
-
-        #endregion
-
-        #region Instance Method: LoadDashboardDevicePaneData
 
         [HttpGet]
         [WebApiRequirePermission(Permission.ViewTelemetry)]
@@ -222,9 +200,5 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
                 });
             return Json(new { mapApiKey = keySetting }, JsonRequestBehavior.AllowGet);
         }
-
-        #endregion
-
-        #endregion
     }
 }
