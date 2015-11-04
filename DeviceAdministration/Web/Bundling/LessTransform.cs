3ef4feae664e3f0a4fe9a8b2645b015465ba7ab0
alzaslon <alzaslon@microsodt.com>
2015-11-04 14:24:14 -0800

    Fixed bundling for Release build.

---
+++ DeviceAdministration/Web/Bundling/LessTransform.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Bundling
+{
+    using System.IO;
+    using System.Web.Optimization;
+    using dotless.Core;
+
+    //public sealed class LessTransform : IBundleTransform
+    //{
+    //    public void Process(BundleContext context, BundleResponse response)
+    //    {
+    //        response.Content = dotless.Core.Less.Parse(response.Content);
+    //        response.ContentType = "text/css";
+    //    }
+    //}
+
+    public class LessTransform : IBundleTransform
+    {
+        private readonly string path;
+
+        public LessTransform(string path)
+        {
+            this.path = path;
+        }
+
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            var oldPath = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(this.path);
+
+            response.Content = Less.Parse(response.Content);
+            Directory.SetCurrentDirectory(oldPath);
+            response.ContentType = "text/css";
+        }
+    }
+}
