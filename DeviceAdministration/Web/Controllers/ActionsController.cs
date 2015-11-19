530d5860d3698d1d0d080f943e68527e54f19567
Jamie Wilbraham <jamie.wilbraham@intergen.co.nz>
2015-11-19 15:45:29 +1300

    minor tweaks

--- DeviceAdministration/Web/Controllers/ActionsController.cs
+++ DeviceAdministration/Web/Controllers/ActionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using DeviceManagement.Infrustructure.Connectivity.Services;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Models;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Repository;
