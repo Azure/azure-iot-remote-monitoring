05c5325dc30d5da5b3fafc487e2fe79d94a6fd42
ersek.attila@hotmail.com <ersek.attila@hotmail.com>
2016-02-24 18:13:14 +0100

    added skeleton for sample device project

---
+++ Device/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Device
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //A class which wraps the barometric sensor
+        BMP280 BMP280;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //This method will be called by the application framework when the page is first loaded
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            MakePinWebAPICall();
+
+            try
+            {
+                //Create a new object for our barometric sensor class
+                BMP280 = new BMP280();
+                //Initialize the sensor
+                await BMP280.Initialize();
+
+                //Create variables to store the sensor data: temperature, pressure and altitude. 
+                //Initialize them to 0.
+                float temp = 0;
+                float pressure = 0;
+                float altitude = 0;
+
+                //Create a constant for pressure at sea level. 
+                //This is based on your local sea level pressure (Unit: Hectopascal)
+                const float seaLevelPressure = 1013.25f;
+
+                //Read 10 samples of the data
+                for (int i = 0; i < 10; i++)
+                {
+                    temp = await BMP280.ReadTemperature();
+                    pressure = await BMP280.ReadPreasure();
+                    altitude = await BMP280.ReadAltitude(seaLevelPressure);
+
+                    //Write the values to your debug console
+                    Debug.WriteLine("Temperature: " + temp.ToString() + " deg C");
+                    Debug.WriteLine("Pressure: " + pressure.ToString() + " Pa");
+                    Debug.WriteLine("Altitude: " + altitude.ToString() + " m");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        // This method will put your pin on the world map of makers using this lesson.
+        // This uses imprecise location (for example, a location derived from your IP 
+        // address with less precision such as at a city or postal code level). 
+        // No personal information is stored.  It simply
+        // collects the total count and other aggregate information.
+        // http://www.microsoft.com/en-us/privacystatement/default.aspx
+        // Comment out the line below to opt-out
+        /// </summary>
+        public void MakePinWebAPICall()
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                // Comment this line to opt out of the pin map.
+                client.GetStringAsync("http://adafruitsample.azurewebsites.net/api?Lesson=203");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Web call failed: " + e.Message);
+            }
+        }
+
+    }
+}
