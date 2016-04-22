9cffe7472035a1258327469ce83bcbc0cc61c132
alzaslon <alzaslon@microsodt.com>
2016-04-22 15:45:50 -0700

    workaround for dotLess handler issues on Azure Websites

--- DeviceAdministration/Web/App_Start/BundleConfig.cs
+++ DeviceAdministration/Web/App_Start/BundleConfig.cs
@@ -39,13 +39,16 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web
                 "~/content/themes/base/dialog.css",
                 "~/content/styles/visuals.min.css"));
 
-            var lessBundle = new Bundle("~/content/css")
-                .Include("~/Content/styles/main.less");
+            //var lessBundle = new Bundle("~/content/css")
+            //    .Include("~/Content/styles/main.less");
 
-            lessBundle.Transforms.Add(new LessTransform(HttpContext.Current.Server.MapPath("~/Content/styles")));
-            lessBundle.Transforms.Add(new CssMinify());
+            //lessBundle.Transforms.Add(new LessTransform(HttpContext.Current.Server.MapPath("~/Content/styles")));
+            //lessBundle.Transforms.Add(new CssMinify());
 
-            bundles.Add(lessBundle);
+            //bundles.Add(lessBundle);
+
+            bundles.Add(new StyleBundle("~/content/css")
+                .Include("~/content/styles/main.css"));
         }
     }
 }
