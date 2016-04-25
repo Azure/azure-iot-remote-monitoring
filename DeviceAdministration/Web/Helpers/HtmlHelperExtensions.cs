0cedc796146400dd491337ba338f481647c0bdfa
alzaslon <alzaslon@microsodt.com>
2016-04-25 15:18:52 -0700

    Localization fixes

---
+++ DeviceAdministration/Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Helpers
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString JavaScriptString(this HtmlHelper htmlHelper, string message)
+        {
+            return htmlHelper.Raw(HttpUtility.JavaScriptStringEncode(message));
+        }
+    }
+}
