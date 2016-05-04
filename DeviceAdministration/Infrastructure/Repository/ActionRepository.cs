799f273442637d4f703e43fac118b79a41755b2e
Ariel Aguilar <araguila@microsoft.com>
2016-05-04 12:20:06 -0700

    Updating ActionRepository.cs to support https post trigger

--- DeviceAdministration/Infrastructure/Repository/ActionRepository.cs
+++ DeviceAdministration/Infrastructure/Repository/ActionRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Repository
@@ -9,24 +11,46 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infr
     /// </summary>
     public class ActionRepository : IActionRepository
     {
-        // Currently this list is not editable in the app
-        private List<string> _actionIds = new List<string>()
-        {
-            "Send Message",
-            "Raise Alarm"
-        };
+        // Currently this dictionary is not editable in the app
+        private Dictionary<string,string> actionIds = new Dictionary<string, string>()
+         {
+            { "Send Message", "" },
+            { "Raise Alarm", "" }
+      };	         
 
         public async Task<List<string>> GetAllActionIdsAsync()
         {
-            return await Task.Run(() => { return _actionIds; });
+            return await Task.Run(() => { return new List<string>(actionIds.Keys); });
         }
 
         public async Task<bool> ExecuteLogicAppAsync(string actionId, string deviceId, string measurementName, double measuredValue)
         {
-            Debug.WriteLine("ExecuteLogicAppAsync is not yet implemented");
-
-            await Task.Run(() => { });
-            return false;
+            if(actionIds.ContainsKey(actionId) && !string.IsNullOrEmpty(actionIds[actionId]))
+            {
+                return await Task.Run(async () =>
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.PostAsync(actionIds[actionId],
+                            new StringContent(JsonConvert.SerializeObject(
+                                new
+                                {
+                                    deviceId = deviceId,
+                                    measurementName = measurementName,
+                                    measuredValue = measuredValue
+                                }),
+                                 System.Text.Encoding.UTF8,
+                                 "application/json"));
+                        return response.IsSuccessStatusCode;
+                    }
+                });
+            }
+            else 
+            {
+                Trace.TraceWarning("ExecuteLogicAppAsync no event endpoint defined for actionId '{0}'", actionId); 
+                return false;
+            }
+            
         }
     }
 }
