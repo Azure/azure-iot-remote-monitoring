4a9a8cb1a061187e952ee49955f1097dc209859c
handeadiguzel <haadig@microsoft.com>
2016-07-15 12:30:12 -0700

    Add validation for dynamic to strongly typed conversion

--- DeviceAdministration/Web/Helpers/DynamicConverter.cs
+++ DeviceAdministration/Web/Helpers/DynamicConverter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using GlobalResources;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Models;
 
@@ -11,13 +13,67 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
     {
         public static T ValidateAndConvert<T>(dynamic dynamicObj)
         {
+
             string dynamicObjStr = Newtonsoft.Json.JsonConvert.SerializeObject(dynamicObj);
             T strongObj = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(dynamicObjStr);
             var strongObjStr = Newtonsoft.Json.JsonConvert.SerializeObject(strongObj);
-
-            //if (strongObjStr != dynamicObjStr)
-            //    throw new Exception(string.Format("Conversion failed for type: {0}", typeof(T)));
+            if (!Validate<T>(dynamicObj, strongObj))
+            {
+                throw new Exception(string.Format("Conversion failed for type: {0}", typeof(T)));
+            }
             return strongObj;
+        }
+
+
+        public static bool Validate<T>(dynamic dynamicObject, T typedObject)
+        {
+            if (dynamicObject == null && typedObject == null)
+            {
+                return true;
+            }
+            else if ((dynamicObject == null && typedObject != null) || (dynamicObject != null && typedObject == null))
+            {
+                return false;
+            }
+            else
+            {
+                if (dynamicObject is IDynamicMetaObjectProvider)
+                {
+                    bool passed = true;
+                    IEnumerable<string> members = dynamicObject.GetDynamicMemberNames();
+                    //foreach (KeyValuePair<string, object> kvp in dynamicObject)
+                    foreach (string prop in members)
+                    {
+                        var dynamicValue = dynamicObject.GetType().GetProperty(prop).GetValue(dynamicObject, null);
+
+                        //string prop = kvp.Key;
+                        var typedProp = typedObject.GetType().GetField(prop);
+                        if (typedProp != null)
+                        {
+                            var typedType = typedObject.GetType();
+                            Type nestedType = typedProp.GetValue(typedObject).GetType();
+                            MethodInfo method = typeof(DynamicConverter).GetMethod("Validate").MakeGenericMethod(new Type[] { nestedType });
+                            var typedValue = Convert.ChangeType(typedProp.GetValue(typedObject), nestedType);
+                            //var dynamicValue = kvp.Value;
+                            passed = (bool)method.Invoke(null, new object[] { dynamicValue, typedValue });
+                            if (!passed)
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    return (dynamicObject == typedObject);
+                }
+            }
+            return true;
         }
     }
 }
