6c7d8b4d0c0478730d9dabdc6bd322276cacd45c
handeadiguzel <haadig@microsoft.com>
2016-07-22 11:42:48 -0700

    Update AdvancedController.cs to use non-dynamic

--- DeviceAdministration/Web/Helpers/CellularExtensions.cs
+++ DeviceAdministration/Web/Helpers/CellularExtensions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using DeviceManagement.Infrustructure.Connectivity.Models.TerminalDevice;
 using DeviceManagement.Infrustructure.Connectivity.Services;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Helpers
 {
@@ -14,21 +15,37 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
             return fullIccidList.Except(usedIccidList);
         }
 
-        public static IEnumerable<string> GetListOfAvailableDeviceIDs(this IExternalCellularService cellularService, List<dynamic> devices)
-        {
-            return (from device in devices
-                    where (device.DeviceProperties != null && device.DeviceProperties.DeviceID != null) &&
-                        (device.SystemProperties == null || device.SystemProperties.ICCID == null)
-                    select device.DeviceProperties.DeviceID.Value
-                    ).Cast<string>().ToList();
-        }
-
         private static IEnumerable<Iccid> GetUsedIccidList(List<dynamic> devices)
         {
             return (from device in devices
                     where (device.DeviceProperties != null && device.DeviceProperties.DeviceID != null) &&
                         (device.SystemProperties != null && device.SystemProperties.ICCID != null)
                     select new Iccid(device.SystemProperties.ICCID.Value)
+                    ).ToList();
+        }
+
+        public static IEnumerable<string> GetListOfAvailableIccidsND(this IExternalCellularService cellularService, IList<DeviceND> devices)
+        {
+            var fullIccidList = cellularService.GetTerminals().Select(i => i.Id);
+            var usedIccidList = GetUsedIccidListND(devices).Select(i => i.Id);
+            return fullIccidList.Except(usedIccidList);
+        }
+
+        public static IEnumerable<string> GetListOfAvailableDeviceIDs(this IExternalCellularService cellularService, IList<DeviceND> devices)
+        {
+            return (from device in devices
+                    where (device.DeviceProperties != null && device.DeviceProperties.DeviceID != null) &&
+                        (device.SystemProperties == null || device.SystemProperties.ICCID == null)
+                    select device.DeviceProperties.DeviceID
+                    ).Cast<string>().ToList();
+        }
+
+        private static IEnumerable<Iccid> GetUsedIccidListND(IList<DeviceND> devices)
+        {
+            return (from device in devices
+                    where (device.DeviceProperties != null && device.DeviceProperties.DeviceID != null) &&
+                        (device.SystemProperties != null && device.SystemProperties.ICCID != null)
+                    select new Iccid(device.SystemProperties.ICCID)
                     ).ToList();
         }
     }
