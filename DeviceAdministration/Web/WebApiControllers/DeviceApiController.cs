cdd2de9f9db228a5461261d08e75ab77fac6170b
handeadiguzel <haadig@microsoft.com>
2016-07-22 11:12:10 -0700

    Move DataTablesResponseND to its own class

--- DeviceAdministration/Web/WebApiControllers/DeviceApiController.cs
+++ DeviceAdministration/Web/WebApiControllers/DeviceApiController.cs
@@ -293,12 +293,4 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.
 #endif
 
     }
-
-    public class DataTablesResponseND
-    {
-        public int Draw { get; set; }
-        public int RecordsTotal { get; set; }
-        public int RecordsFiltered { get; set; }
-        public DeviceND[] Data { get; set; }
-    }
 }
