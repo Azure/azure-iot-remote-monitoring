51198c6252b012cfc05ff45ed7ba6dc34b08b07d
Kapil Agarwal <kaagarw@microsoft.com>
2016-07-26 11:40:28 -0700

    Adding ND classes to simulator

---
+++ Simulator/Simulator.WebJob/SimulatorCore/Devices/DeviceBaseND.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.DeviceSchema;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Factory;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models.Commands;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.CommandProcessors;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Logging;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Telemetry;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Telemetry.Factory;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport.Factory;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Devices
+{
+    /// <summary>
+    /// Simulates a single IoT device that sends and recieves data from a transport
+    /// </summary>
+    public class DeviceBaseND : IDeviceND
+    {
+        // pointer to the currently executing event group
+        private int _currentEventGroup = 0;
+
+        protected readonly ILogger Logger;
+        protected readonly ITransportFactoryND TransportFactory;
+        protected readonly ITelemetryFactoryND TelemetryFactory;
+        protected readonly IConfigurationProvider ConfigProvider;
+        protected ITransportND Transport;
+        protected CommandProcessorND RootCommandProcessor;
+
+        public string DeviceID
+        {
+            get { return DeviceProperties.DeviceID; }
+            set { DeviceProperties.DeviceID = value; }
+        }
+
+        public string HostName { get; set; }
+        public string PrimaryAuthKey { get; set; }
+
+        private DeviceProperties _deviceProperties;
+        public DeviceProperties DeviceProperties
+        {
+            get { return _deviceProperties; }
+            set { _deviceProperties = value; }
+        }
+
+        public List<Command> Commands { get; set; }
+
+        public dynamic Telemetry { get; set; }
+
+        public List<ITelemetryND<DeviceND>> TelemetryEvents { get; private set; }
+        public bool RepeatEventListForever { get; set; }
+
+        protected object _telemetryController;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger">Logger where this device will log information to</param>
+        /// <param name="transport">Transport where the device will send and receive data to/from</param>
+        /// <param name="config">Config to start this device with</param>
+        public DeviceBaseND(ILogger logger, ITransportFactoryND transportFactory, ITelemetryFactoryND telemetryFactory, IConfigurationProvider configurationProvider)
+        {
+            ConfigProvider = configurationProvider;
+            Logger = logger;
+            TransportFactory = transportFactory;
+            TelemetryFactory = telemetryFactory;
+            TelemetryEvents = new List<ITelemetryND<DeviceND>>();
+        }
+
+        public void Init(InitialDeviceConfig config)
+        {
+            InitDeviceInfo(config);
+
+            Transport = TransportFactory.CreateTransport(this);
+            _telemetryController = TelemetryFactory.PopulateDeviceWithTelemetryEvents(this);
+
+            InitCommandProcessors();
+        }
+
+        protected virtual void InitDeviceInfo(InitialDeviceConfig config)
+        {
+            DeviceND initialDevice = SampleDeviceFactory.GetSampleSimulatedDevice(config.DeviceId, config.Key);
+            DeviceProperties = DeviceSchemaHelper.GetDeviceProperties(initialDevice);
+            Commands = CommandSchemaHelper.GetSupportedCommands(initialDevice);
+            Telemetry = CommandSchemaHelper.GetTelemetrySchema(initialDevice);
+            HostName = config.HostName;
+            PrimaryAuthKey = config.Key;
+        }
+
+        /// <summary>
+        /// Builds up a set of commands supported by this device
+        /// </summary>
+        protected virtual void InitCommandProcessors()
+        {
+            var pingDeviceProcessor = new PingDeviceProcessorND(this);
+
+            RootCommandProcessor = pingDeviceProcessor;
+        }
+
+        public async virtual Task SendDeviceInfo()
+        {
+            Logger.LogInfo("Sending Device Info for device {0}...", DeviceID);
+            await Transport.SendEventAsync(GetDeviceInfo());
+        }
+
+        /// <summary>
+        /// Generates a DeviceInfo packet for a simulated device to send over the wire
+        /// </summary>
+        /// <returns></returns>
+        public virtual DeviceND GetDeviceInfo()
+        {
+            DeviceND device = DeviceSchemaHelperND.BuildDeviceStructure(DeviceID, true, null);
+            device.DeviceProperties = this.DeviceProperties;
+            device.Commands = CommandSchemaHelper.GetSupportedCommands(this);
+            device.Telemetry = CommandSchemaHelper.GetTelemetrySchema(this);
+            device.Version = SampleDeviceFactory.VERSION_1_0;
+            device.ObjectType = SampleDeviceFactory.OBJECT_TYPE_DEVICE_INFO;
+
+            // Remove the system properties from a device, to better emulate the behavior of real devices when sending device info messages.
+            DeviceSchemaHelperND.RemoveSystemPropertiesForSimulatedDeviceInfo(device);
+
+            return device;
+        }
+
+        /// <summary>
+        /// Starts the send event loop and runs the receive loop in the background
+        /// to listen for commands that are sent to the device
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task StartAsync(CancellationToken token)
+        {
+            try
+            {
+                Transport.Open();
+
+                var loopTasks = new List<Task>
+                {
+                    StartReceiveLoopAsync(token),
+                    StartSendLoopAsync(token)
+                };
+
+                // Wait both the send and receive loops
+                await Task.WhenAll(loopTasks.ToArray());
+
+                // once the code makes it here the token has been canceled
+                await Transport.CloseAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Unexpected Exception starting device: {0}", ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Iterates through the list of IEventGroups and fires off the events in a given event group before moving to the next.
+        /// If RepeatEventListForever is true the device will continue to loop through each event group, if false
+        /// once a single pass is made through all event groups the device will stop sending events
+        /// </summary>
+        /// <param name="token">Cancellation token to cancel out of the loop</param>
+        /// <returns></returns>
+        private async Task StartSendLoopAsync(CancellationToken token)
+        {
+            try
+            {
+                Logger.LogInfo("Booting device {0}...", DeviceID);
+
+                do
+                {
+                    _currentEventGroup = 0;
+
+                    Logger.LogInfo("Starting events list for device {0}...", DeviceID);
+
+                    while (_currentEventGroup < TelemetryEvents.Count && !token.IsCancellationRequested)
+                    {
+                        Logger.LogInfo("Device {0} starting IEventGroup {1}...", DeviceID, _currentEventGroup);
+
+                        var eventGroup = TelemetryEvents[_currentEventGroup];
+
+                        await eventGroup.SendEventsAsync(token, async (DeviceND eventData) =>
+                        {
+                            await Transport.SendEventAsync(eventData);
+                        });
+
+                        _currentEventGroup++;
+                    }
+
+                    Logger.LogInfo("Device {0} finished sending all events in list...", DeviceID);
+
+                } while (RepeatEventListForever && !token.IsCancellationRequested);
+
+                Logger.LogWarning("Device {0} sent all events and is shutting down send loop. (Set RepeatEventListForever = true on the device to loop forever.)", DeviceID);
+
+            }
+            catch (TaskCanceledException)
+            {
+                //do nothing if the task was cancelled
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Unexpected Exception starting device send loop: {0}", ex.ToString());
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                Logger.LogInfo("********** Processing Device {0} has been cancelled - StartSendLoopAsync Ending. **********", DeviceID);
+            }
+        }
+
+        /// <summary>
+        /// Starts the loop that listens for events/commands from the IoT Hub to be sent to this device
+        /// </summary>
+        /// <param name="token">Cancellation token that can stop the loop if needed</param>
+        private async Task StartReceiveLoopAsync(CancellationToken token)
+        {
+            DeserializableCommand command;
+            Exception exception;
+            CommandProcessingResultND processingResult;
+
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    command = null;
+                    exception = null;
+
+                    // Pause before running through the receive loop
+                    await Task.Delay(TimeSpan.FromSeconds(10), token);
+                    Logger.LogInfo("Device {0} checking for commands...", DeviceID);
+
+                    try
+                    {
+                        // Retrieve the message from the IoT Hub
+                        command = await Transport.ReceiveAsync();
+
+                        if (command == null)
+                        {
+                            continue;
+                        }
+
+                        processingResult =
+                        await RootCommandProcessor.HandleCommandAsync(command);
+
+                        switch (processingResult)
+                        {
+                            case CommandProcessingResultND.CannotComplete:
+                                await Transport.SignalRejectedCommand(command);
+                                break;
+
+                            case CommandProcessingResultND.RetryLater:
+                                await Transport.SignalAbandonedCommand(command);
+                                break;
+
+                            case CommandProcessingResultND.Success:
+                                await Transport.SignalCompletedCommand(command);
+                                break;
+                        }
+
+                        Logger.LogInfo(
+                            "Device: {1}{0}Command: {2}{0}Lock token: {3}{0}Result: {4}{0}",
+                            Console.Out.NewLine,
+                            DeviceID,
+                            command.CommandName,
+                            command.LockToken,
+                            processingResult);
+                    }
+                    catch (IotHubException ex)
+                    {
+                        exception = ex;
+
+                        Logger.LogInfo(
+                            "Device: {1}{0}Command: {2}{0}Lock token: {3}{0}Error Type: {4}{0}Exception: {5}{0}",
+                            Console.Out.NewLine,
+                            DeviceID,
+                            command.CommandName,
+                            command.LockToken,
+                            ex.IsTransient ? "Transient Error" : "Non-transient Error",
+                            ex.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+
+                        Logger.LogInfo(
+                            "Device: {1}{0}Command: {2}{0}Lock token: {3}{0}Exception: {4}{0}",
+                            Console.Out.NewLine,
+                            DeviceID,
+                            command.CommandName,
+                            command.LockToken,
+                            ex.ToString());
+                    }
+
+                    if ((command != null) &&
+                        (exception != null))
+                    {
+                        await Transport.SignalAbandonedCommand(command);
+                    }
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                //do nothing if the task was cancelled
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Unexpected Exception starting device receive loop: {0}", ex.ToString());
+            }
+
+            Logger.LogInfo("********** Processing Device {0} has been cancelled - StartReceiveLoopAsync Ending. **********", DeviceID);
+        }
+    }
+}
---
+++ Simulator/Simulator.WebJob/SimulatorCore/Transport/ITransportND.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport
+{
+    /// <summary>
+    /// Interface to provide actions that can be performed against a cloud service such as IoT Hub
+    /// </summary>
+    public interface ITransportND
+    {
+        void Open();
+        Task CloseAsync();
+
+        Task SendEventAsync(DeviceND eventData);
+
+        Task SendEventAsync(Guid eventId, DeviceND eventData);
+
+        Task SendEventBatchAsync(IEnumerable<Client.Message> messages);
+
+        Task<DeserializableCommand> ReceiveAsync();
+
+        Task SignalAbandonedCommand(DeserializableCommand command);
+
+        Task SignalCompletedCommand(DeserializableCommand command);
+
+        Task SignalRejectedCommand(DeserializableCommand command);
+    }
+}
