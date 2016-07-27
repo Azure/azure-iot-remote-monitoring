04faab3be22dfe2a3aa1113b3cda74a4f0fdea8b
Kapil Agarwal <kaagarw@microsoft.com>
2016-07-27 11:26:21 -0700

    Added DeserializableCommandND

---
+++ Simulator/Simulator.WebJob/SimulatorCore/Transport/DeserializableCommandND.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Serialization;
+using System;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models.Commands;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport
+{
+    /// <summary>
+    /// Wraps the byte array returned from the cloud so that it can be deserialized
+    /// </summary>
+    public class DeserializableCommandND
+    {
+        private readonly Command _command;
+        private readonly string _lockToken;
+
+        public string CommandName
+        {
+            get { return _command.Name; }
+        }
+
+        public DeserializableCommandND(Client.Message message, ISerialize serializer)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException("message");
+            }
+
+            if (serializer == null)
+            {
+                throw new ArgumentNullException("serializer");
+            }
+
+            Debug.Assert(
+                !string.IsNullOrEmpty(message.LockToken),
+                "message.LockToken is a null reference or empty string.");
+            _lockToken = message.LockToken;
+
+            byte[] messageBytes = message.GetBytes(); // this needs to be saved if needed later, because it can only be read once from the original Message
+
+            _command = serializer.DeserializeObject<Command>(messageBytes);
+        }
+
+        public Command Command
+        {
+            get { return _command; }
+        }
+
+        public string LockToken
+        {
+            get
+            {
+                return _lockToken;
+            }
+        }
+    }
+}
