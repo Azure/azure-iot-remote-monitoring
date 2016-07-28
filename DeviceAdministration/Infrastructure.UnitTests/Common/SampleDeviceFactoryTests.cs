169711bf5c202c1631016c9ae90f3307b4366f9b
Karan <kavishw@microsoft.com>
2016-07-28 15:30:07 -0700

    xunit test

---
+++ DeviceAdministration/Infrastructure.UnitTests/Common/SampleDeviceFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Common
+{
+    public class SampleDeviceFactoryTests
+    {
+        [Fact]
+        public void TestGetSampleSimulatedDevice()
+        {
+            
+        }
+
+        [Fact]
+        public void TestGetSampleDevice()
+        {
+
+        }
+
+    }
+}
---
+++ DeviceAdministration/Infrastructure.UnitTests/SampleDeviceFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.DeviceSchema;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests
+{
+    [TestFixture]
+    public class SampleDeviceFactoryTests
+    {
+        private Device d;
+        [SetUp]
+        public void BuildDevice()
+        {
+            d = DeviceSchemaHelper.BuildDeviceStructure("test", true, null);
+
+        }
+        [Test]
+        public void testGetSampleSimulatedDevice()
+        {
+
+        }
+
+    }
+}
