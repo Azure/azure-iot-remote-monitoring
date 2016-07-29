8e0bc5177f3bc430a77901dd8452e1751406eeb0
Karan <kavishw@microsoft.com>
2016-07-29 10:11:51 -0700

    SampleFactoryTests

--- DeviceAdministration/Infrastructure.UnitTests/Common/SampleDeviceFactoryTests.cs
+++ DeviceAdministration/Infrastructure.UnitTests/Common/SampleDeviceFactoryTests.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.DeviceSchema;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Factory;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
 using Xunit;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Common
 {
     public class SampleDeviceFactoryTests
     {
+        private ISecurityKeyGenerator _securityKeys;
         [Fact]
         public void TestGetSampleSimulatedDevice()
         {
-            
+            //not null
+            DeviceModel d = DeviceSchemaHelper.BuildDeviceStructure("test", true, null);
+
+            Assert.NotNull(d);
+            Assert.Equal("test", d.DeviceProperties.DeviceID);
+            Assert.Equal("normal", d.DeviceProperties.DeviceState);
+            Assert.Equal(null, d.DeviceProperties.HubEnabledState);
+
         }
 
         [Fact]
         public void TestGetSampleDevice()
         {
-
+            Random randomnumber = new Random();
+            _securityKeys = new SecurityKeyGenerator();
+            SecurityKeys keys = _securityKeys.CreateRandomKeys();
+            DeviceModel d = SampleDeviceFactory.GetSampleDevice(randomnumber, keys);
+            Assert.NotNull(d);
+            Assert.NotNull(d.DeviceProperties);
+            Assert.NotNull(d.DeviceProperties.DeviceID);
         }
+
+        [Fact]
+        public void TestGetDefaultDeviceNames()
+        {
+            List<String> s = SampleDeviceFactory.GetDefaultDeviceNames();
+            Assert.NotNull(s);
+        }
+        
 
     }
 }
