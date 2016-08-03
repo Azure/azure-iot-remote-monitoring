0e94ec30583834b9f45be620fd57396a82abf725
Karan <kavishw@microsoft.com>
2016-08-03 12:55:13 -0700

    adding device base tests

--- DeviceAdministration/Infrastructure.UnitTests/Simulator.WebJob/DeviceBaseTests.cs
+++ DeviceAdministration/Infrastructure.UnitTests/Simulator.WebJob/DeviceBaseTests.cs
@@ -1,35 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Configurations;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models.Commands;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.CommandProcessors;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Devices;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Logging;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Serialization;
-using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Telemetry;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Telemetry.Factory;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport.Factory;
-using Mono.Security.Authenticode;
 using Moq;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Simulator.WebJob
 {
     public class DeviceBaseTests
     {
-        private Mock<ILogger> _loggerMock;
-        private Mock<ITransportFactory> _transportFactory;
-        private Mock<ITelemetryFactory> _telemetryFactoryMock;
-        private Mock<IConfigurationProvider> _configurationProviderMock;
-        private Mock<ITransport> _transport;
-        private DeviceBase deviceBase;
-
         public DeviceBaseTests()
         {
             _loggerMock = new Mock<ILogger>();
@@ -37,13 +19,34 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infr
             _telemetryFactoryMock = new Mock<ITelemetryFactory>();
             _configurationProviderMock = new Mock<IConfigurationProvider>();
             _transport = new Mock<ITransport>();
-            deviceBase = new DeviceBase(_loggerMock.Object, _transportFactory.Object, _telemetryFactoryMock.Object, _configurationProviderMock.Object) ;
+            deviceBase = new DeviceBase(_loggerMock.Object, _transportFactory.Object, _telemetryFactoryMock.Object,
+                _configurationProviderMock.Object);
+        }
+
+        private readonly Mock<ILogger> _loggerMock;
+        private readonly Mock<ITransportFactory> _transportFactory;
+        private readonly Mock<ITelemetryFactory> _telemetryFactoryMock;
+        private readonly Mock<IConfigurationProvider> _configurationProviderMock;
+        private Mock<ITransport> _transport;
+        private readonly DeviceBase deviceBase;
+
+        [Fact]
+        public void GetDeviceInfoTests()
+        {
+            var config = new InitialDeviceConfig();
+            config.HostName = "HostName";
+            config.DeviceId = "test";
+            config.Key = "key";
+
+            deviceBase.Init(config);
+            var device = deviceBase.GetDeviceInfo();
+            Assert.Equal(device.DeviceProperties.DeviceID, "test");
         }
 
         [Fact]
         public void InitTests()
         {
-            InitialDeviceConfig config = new InitialDeviceConfig();
+            var config = new InitialDeviceConfig();
             config.HostName = "HostName";
             config.DeviceId = "test";
             config.Key = "key";
@@ -55,24 +58,7 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infr
             Assert.Equal(deviceBase.PrimaryAuthKey, "key");
             Assert.NotNull(deviceBase.DeviceProperties);
             Assert.NotNull(deviceBase.Commands);
-            Assert.NotNull(deviceBase.Telemetry); 
+            Assert.NotNull(deviceBase.Telemetry);
         }
-
-        [Fact]
-        public void GetDeviceInfoTests()
-        {
-            InitialDeviceConfig config = new InitialDeviceConfig();
-            config.HostName = "HostName";
-            config.DeviceId = "test";
-            config.Key = "key";
-
-            deviceBase.Init(config);
-            DeviceModel device = deviceBase.GetDeviceInfo();
-            Assert.Equal(device.DeviceProperties.DeviceID, "test");
-        }
-
-
-
-
     }
 }
