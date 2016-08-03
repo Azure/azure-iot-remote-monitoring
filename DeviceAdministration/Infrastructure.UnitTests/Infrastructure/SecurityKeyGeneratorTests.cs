8b9ee7082969af0ab4b7b68141dd22d56bb1032c
Karan <kavishw@microsoft.com>
2016-08-03 10:36:25 -0700

    securityKeyGenTests

---
+++ DeviceAdministration/Infrastructure.UnitTests/Infrastructure/SecurityKeyGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests
+{
+    public class SecurityKeyGeneratorTests
+    {
+        [Fact]
+        public void CreateRandomKeysTest()
+        {
+            var securityKeyGenerator = new SecurityKeyGenerator();
+            var keys = securityKeyGenerator.CreateRandomKeys();
+
+            Assert.NotNull(keys);
+            Assert.NotEqual(keys.PrimaryKey, keys.SecondaryKey);
+        }
+    }
+}
