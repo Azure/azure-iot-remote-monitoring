f43071e4fb6700bc6e61ab4b5f14578549624233
handeadiguzel <haadig@microsoft.com>
2016-08-03 10:36:10 -0700

    Add tests for JasperCredentialProvider

---
+++ DeviceAdministration/Infrastructure.UnitTests/Web/Helpers/JasperCredentialProviderTests.cs
@@ -0,0 +1,37 @@
+﻿using DeviceManagement.Infrustructure.Connectivity.Models.Jasper;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Repository;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Helpers;
+using Moq;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Web.Helpers
+{
+    public class JasperCredentialProviderTests
+    {
+        private JasperCredentialsProvider provider;
+        private readonly Mock<IApiRegistrationRepository> apiRegMock;
+        private readonly IFixture fixture;
+
+        public JasperCredentialProviderTests()
+        {
+            this.apiRegMock = new Mock<IApiRegistrationRepository>();
+            this.provider = new JasperCredentialsProvider(this.apiRegMock.Object);
+            this.fixture = new Fixture();
+        }
+
+        [Fact]
+        public void ProvideTest()
+        {
+            var apiReg = this.fixture.Create<ApiRegistrationModel>();
+            this.apiRegMock.Setup(mock => mock.RecieveDetails()).Returns(apiReg);
+            var result = this.provider.Provide();
+            var res = result as JasperCredentials;
+            Assert.Equal(res.BaseUrl, apiReg.BaseUrl);
+            Assert.Equal(res.LicenceKey, apiReg.LicenceKey);
+            Assert.Equal(res.Password, apiReg.Password);
+            Assert.Equal(res.Username, apiReg.Username);
+        }
+    }
+}
