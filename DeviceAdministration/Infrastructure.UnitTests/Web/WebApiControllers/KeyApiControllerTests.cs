c7012cee778a7f1b5ae748460ba6b59c1a0a6766
handeadiguzel <haadig@microsoft.com>
2016-08-03 17:05:57 -0700

    Add tests for DeviceTypesController and KeyApiController

---
+++ DeviceAdministration/Infrastructure.UnitTests/Web/WebApiControllers/KeyApiControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.BusinessLogic;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.WebApiControllers;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Web.WebApiControllers
+{
+    public class KeyApiControllerTests
+    {
+        private readonly KeyApiController keyApiController;
+        private readonly IKeyLogic keyLogic;
+        private readonly ISecurityKeyGenerator securityKeyGenerator;
+
+        public KeyApiControllerTests()
+        {
+            this.securityKeyGenerator = new SecurityKeyGenerator();
+            this.keyLogic = new KeyLogic(this.securityKeyGenerator);
+            this.keyApiController = new KeyApiController(this.keyLogic);
+            this.keyApiController.InitializeRequest();
+        }
+
+        [Fact]
+        public async void GetKeysAsyncTest()
+        {
+            var res = await this.keyApiController.GetKeysAsync();
+            res.AssertOnError();
+            var data = res.ExtractContentDataAs<SecurityKeys>();
+            Assert.NotNull(data);
+            Assert.NotNull(data.PrimaryKey);
+            Assert.NotNull(data.SecondaryKey);
+        }
+    }
+}
