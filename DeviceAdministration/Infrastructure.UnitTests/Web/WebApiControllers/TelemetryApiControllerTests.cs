456ee9698439ae0e637d263dfbc1a9c19b1b3c37
handeadiguzel <haadig@microsoft.com>
2016-08-04 11:35:13 -0700

    Add tests for DeviceRulesApiController and TelemetryApiController

---
+++ DeviceAdministration/Infrastructure.UnitTests/Web/WebApiControllers/TelemetryApiControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.UnitTests.Web.WebApiControllers
+{
+    public class TelemetryApiControllerTests
+    {
+    }
+}
