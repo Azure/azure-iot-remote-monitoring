1fc2ed2576093444ef2cf357d3e697737a5def4f
Charles Lipford <charlip@microsoft.com>
2016-08-08 14:46:19 -0700

    finishing off simulartor core tests

---
+++ DeviceAdministration/Infrastructure.UnitTests/Simulator.WebJob/DeserializableCommandTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Serialization;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.Models.Commands;
+using Moq;
+using System.Text;
+using Xunit;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Transport;
+using System;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Simulator.WebJob
+{
+    public class DeserializableCommandTest
+    {
+        private readonly Mock<ISerialize> _serializerMock;
+        private readonly Client.Message _message;
+
+        public DeserializableCommandTest()
+        {
+            _serializerMock = new Mock<ISerialize>();
+            _message = new Client.Message(Encoding.UTF8.GetBytes("{}"));
+        }
+
+        [Fact]
+        public void CreationFailureTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DeserializableCommand(null, _serializerMock.Object));
+            Assert.Throws<ArgumentNullException>(() => new DeserializableCommand(_message, null));
+        }
+    }
+}
---
+++ DeviceAdministration/Infrastructure.UnitTests/Simulator.WebJob/JsonSerializeTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Simulator.WebJob.SimulatorCore.Serialization;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Simulator.WebJob
+{
+    class JsonSerializeTests
+    {
+        private JsonSerialize serializer;
+
+        public JsonSerializeTests() {
+            this.serializer = new JsonSerialize();
+        }
+
+        [Fact]
+        public void EndToEndTest()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            byte[] serializedObject = this.serializer.SerializeObject(list);
+            List<int> deserializedList = this.serializer.DeserializeObject<List<int>>(serializedObject);
+
+            Assert.Equal(list, deserializedList);
+        }
+    }
+}
