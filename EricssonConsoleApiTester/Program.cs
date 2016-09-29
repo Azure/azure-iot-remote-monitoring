cd7e53bac2ebd155e4ae885f64e6fb24005a23d0
Jamie Wilbraham <jamie.wilbraham@intergen.co.nz>
2016-09-29 15:40:04 +1300

    tester changes

--- EricssonConsoleApiTester/Program.cs
+++ EricssonConsoleApiTester/Program.cs
@@ -6,7 +6,9 @@ using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.Web.Services.Protocols;
 using EricssonConsoleApiTester.ApiStatus;
+using EricssonConsoleApiTester.RealtimeTrace;
 using EricssonConsoleApiTester.SubscriptionManagement;
+using EricssonConsoleApiTester.SubscriptionTraffic;
 
 namespace EricssonConsoleApiTester
 {
@@ -16,54 +18,74 @@ namespace EricssonConsoleApiTester
         {
             //echo tester
 
-            var binding = new BasicHttpBinding {Security = {Mode = BasicHttpSecurityMode.Transport}};
-            EndpointAddress endpointAddress = EricssonEndpointBuilder.GetAuthorizedEndpoint("https://serviceportal.telenorconnexion.com/dcpapi/ApiStatus");
-
-            var apiStatusClient = new ApiStatusClient(binding, endpointAddress);
-
-            try
-            {
-                var response1 = apiStatusClient.echo(new echo());
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            var binding = new BasicHttpBinding { Security = { Mode = BasicHttpSecurityMode.Transport } };
+            EndpointAddress endpointAddress = EricssonEndpointBuilder.GetAuthorizedEndpoint("https://orange.dcp.ericsson.net/dcpapi/ApiStatus");
 
 
 
-            //sub tester -get a single sim information
+            //SUBSCRIPTION MANAGEMENT
             var subscriptionManClient = new SubscriptionManagementClient();
             subscriptionManClient.Endpoint.Address =
                 EricssonEndpointBuilder.GetAuthorizedEndpoint(
-                    "https://serviceportal.telenorconnexion.com/dcpapi/SubscriptionManagement");
+                    "https://orange.dcp.ericsson.net/dcpapi/SubscriptionManagement");
 
-            //var response2 = subscriptionManClient.QuerySimResource(new QuerySimResource()
-            //{
-            //    resource = new resource()
-            //    {
-            //        id = "89460800000105696001",
-            //        type = "icc"
-            //    }
-
-            //});
-
-   
-
-
-            var resp = subscriptionManClient.QuerySubscriptions(new QuerySubscriptionsRequest()
+            var response1 = subscriptionManClient.QuerySimResource(new QuerySimResource()
             {
-                maxResults = 10,
-                resource = new resource()
+                resource = new SubscriptionManagement.resource()
                 {
-                    id = "240080000569600",
-                    type = "imsi"
+                    id = "89883011539830007560",
+                    type = "icc"
                 }
 
             });
 
+            var resp = subscriptionManClient.QuerySubscriptions(new QuerySubscriptionsRequest()
+            {
+                maxResults = 10,
+                resource = new SubscriptionManagement.resource()
+                {
+                    id = "901312000000466",
+                    type = "imsi"
+                }
+            });
 
+
+            //Subscription Traffic - potential for activity.
+            var subscriptionTrafficClient = new SubscriptionTrafficClient();
+            subscriptionTrafficClient.Endpoint.Address = EricssonEndpointBuilder.GetAuthorizedEndpoint(
+                    "https://orange.dcp.ericsson.net/dcpapi/SubscriptionTraffic");
+
+            var subResp = subscriptionTrafficClient.query(new query()
+            {
+                resource = new SubscriptionTraffic.resource()
+                {
+                    id = "901312000000466",
+                    type = resourceType.imsi
+                }
+
+            });
+
+            //Realtime Trace - PDP? HLR?
+            var realTimeClient = new RealtimeTraceClient();
+            realTimeClient.Endpoint.Address = EricssonEndpointBuilder.GetAuthorizedEndpoint(
+                "https://orange.dcp.ericsson.net/dcpapi/RealtimeTrace");
+
+            var traceResp = realTimeClient.query(new Query()
+            {
+                traceLength = 5,
+                filter = new Filter()
+                {
+                    
+                    resourceFilter = new ResourceFilter()
+                    {
+                        resourceId = "901312000000466",
+                        resourceType = "imsi",
+                        range = 5
+                    }
+
+                }
+
+            });
 
         }
 
