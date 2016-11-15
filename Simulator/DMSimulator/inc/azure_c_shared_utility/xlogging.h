6bd97fb86eaf1bc912ef942f78db2e21afea589f
Xiangzhi Sheng <xisheng@microsoft.com>
2016-11-15 13:29:12 +0800

    Fix build break due to invalid characters on East Asia code page

--- Simulator/DMSimulator/inc/azure_c_shared_utility/xlogging.h
+++ Simulator/DMSimulator/inc/azure_c_shared_utility/xlogging.h
@@ -41,9 +41,9 @@ typedef void(*LOGGER_LOG)(LOG_CATEGORY log_category, const char* file, const cha
 #define UNUSED(x) (void)(x)
 #elif defined(ARDUINO_ARCH_ESP8266)
 /*
-The ESP8266 compiler don’t do a good job compiling this code, it do not understand that the ‘format’ is
-a ‘cont char*’ and moves it to the RAM as a global variable, increasing a lot the .bss. So, we create a
-specific LogInfo that explicitly pin the ‘format’ on the PROGMEM (flash) using a _localFORMAT variable
+The ESP8266 compiler don’t do a good job compiling this code, it do not understand that the ‘format?is
+a ‘cont char*?and moves it to the RAM as a global variable, increasing a lot the .bss. So, we create a
+specific LogInfo that explicitly pin the ‘format?on the PROGMEM (flash) using a _localFORMAT variable
 with the macro PSTR.
 
 #define ICACHE_FLASH_ATTR   __attribute__((section(".irom0.text")))
@@ -51,7 +51,7 @@ with the macro PSTR.
 #define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
 const char* __localFORMAT = PSTR(FORMAT);
 
-On the other hand, vsprintf do not support the pinned ‘format’ and os_printf do not works with va_list,
+On the other hand, vsprintf do not support the pinned ‘format?and os_printf do not works with va_list,
 so we compacted the log in the macro LogInfo.
 */
 #include "esp8266/azcpgmspace.h"
@@ -67,7 +67,7 @@ so we compacted the log in the macro LogInfo.
 #if defined _MSC_VER
 #define LOG(log_category, log_options, format, ...) { LOGGER_LOG l = xlogging_get_log_function(); if (l != NULL) l(log_category, __FILE__, FUNC_NAME, __LINE__, log_options, format, __VA_ARGS__); }
 #else
-#define LOG(log_category, log_options, format, ...) { LOGGER_LOG l = xlogging_get_log_function(); if (l != NULL) l(log_category, __FILE__, FUNC_NAME, __LINE__, log_options, format, ##__VA_ARGS__); } 
+#define LOG(log_category, log_options, format, ...) { LOGGER_LOG l = xlogging_get_log_function(); if (l != NULL) l(log_category, __FILE__, FUNC_NAME, __LINE__, log_options, format, ##__VA_ARGS__); }
 #endif
 
 #if defined _MSC_VER
