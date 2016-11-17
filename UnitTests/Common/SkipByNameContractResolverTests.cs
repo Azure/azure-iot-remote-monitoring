4c44911319759f4271416646cc15a093a5815f5c
Xiangzhi Sheng <xzsheng@msn.com>
2016-11-17 18:47:46 +0800

    [BugFix] In the device detail panel, incorrect UI control used for DeviceID
    One more minor changes: DeviceID was excluded from the reported properties. To implement it, we added a contract resolver rather than directly put an ignore attribute on property "DeviceProperties.DeviceID", because we want to skip it only when it was serialized as the reported properties.

---
+++ UnitTests/Common/SkipByNameContractResolverTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Applications.RemoteMonitoring.Common.JsonContractResolvers;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Common
+{
+    public class SkipByNameContractResolverTests
+    {
+        [Fact]
+        public void SerializeTest()
+        {
+            var obj = new
+            {
+                FieldA = "string value",
+                FieldB = 1.0,
+                FieldC = DateTime.Now
+            };
+
+            var text = JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings
+            {
+                ContractResolver = new SkipByNameContractResolver("FieldA", "FieldC")
+            });
+
+            Assert.Equal(text, "{\"FieldB\":1.0}");
+        }
+    }
+}
