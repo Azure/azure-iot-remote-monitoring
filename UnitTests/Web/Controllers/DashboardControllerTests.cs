5703934312b0ddc8de182c8e4e0ed7eb9b8bf49f
Ce Li (Chinasoft) <v-celi@microsoft.com>
2017-01-10 12:49:12 +0800

    Fix DashboardConroller index test fail

--- UnitTests/Web/Controllers/DashboardControllerTests.cs
+++ UnitTests/Web/Controllers/DashboardControllerTests.cs
@@ -8,6 +8,8 @@ using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Mode
 using Moq;
 using Ploeh.AutoFixture;
 using Xunit;
+using System.Web;
+using System.Web.Routing;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Web
 {
@@ -24,10 +26,9 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Web
             var deviceTelemetryMock = new Mock<IDeviceTelemetryLogic>();
             _configurationMock = new Mock<IConfigurationProvider>();
             _dashboardController = new DashboardController(
-                _deviceLogicMock.Object, 
-                deviceTelemetryMock.Object, 
+                _deviceLogicMock.Object,
+                deviceTelemetryMock.Object,
                 _configurationMock.Object);
-
             fixture = new Fixture();
         }
 
@@ -36,13 +37,25 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.UnitTests.Web
         {
             var filterMock = fixture.Create<DeviceListFilterResult>();
             var key = fixture.Create<string>();
+            var cookiecollection = new HttpCookieCollection();
+            var request = new Mock<HttpRequestBase>();
+            request.SetupGet(req => req.Cookies).Returns(new HttpCookieCollection());
+            var response = new Mock<HttpResponseBase>();
+            response.SetupGet(res => res.Cookies).Returns(cookiecollection);
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(request.Object);
+            context.SetupGet(x => x.Response).Returns(response.Object);
+
             _deviceLogicMock.Setup(mock => mock.GetDevices(It.IsAny<DeviceListFilter>())).ReturnsAsync(filterMock);
             _configurationMock.Setup(mock => mock.GetConfigurationSettingValue("MapApiQueryKey")).Returns(key);
+            _dashboardController.ControllerContext = new ControllerContext(context.Object, new RouteData(), _dashboardController);
 
             var result = await _dashboardController.Index();
             var view = result as ViewResult;
             var model = view.Model as DashboardModel;
 
+            Assert.Equal(_dashboardController.Response.Cookies.Count, 1);
+            Assert.Equal(_dashboardController.Response.Cookies["_culture"].Value, "en-US");
             Assert.Equal(model.DeviceIdsForDropdown.Count, filterMock.Results.Count);
             var deviceIDs = model.DeviceIdsForDropdown.First();
             var mockDeviceId = filterMock.Results.First().DeviceProperties.DeviceID;
