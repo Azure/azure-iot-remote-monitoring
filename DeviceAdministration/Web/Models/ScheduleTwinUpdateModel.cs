f8e70765002f02e57723086c5bb67b74f831ed2e
Hua Zhang <huazhan@microsoft.com>
2017-01-16 17:55:04 +0800

    Bug fix of treat empty twin value as null
    MVC will treat empty string as null value when submit through form. Add an
    attribute to model to avoid this.

--- DeviceAdministration/Web/Models/ScheduleTwinUpdateModel.cs
+++ DeviceAdministration/Web/Models/ScheduleTwinUpdateModel.cs
@@ -1,37 +1,32 @@
-﻿using GlobalResources;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Infrastructure.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web.Models
 {
     public class DesiredPropetiesEditViewModel
     {
-        public string PropertyName { get; set; }   
+        public string PropertyName { get; set; }
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string PropertyValue { get; set; }
-
         [JsonConverter(typeof(StringEnumConverter))]
         public TwinDataType DataType { get; set; }
         public bool isDeleted { get; set; }
-
     }
 
     public class TagsEditViewModel
     {
         public string TagName { get; set; }
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string TagValue { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         public TwinDataType DataType { get; set; }
         public bool isDeleted { get; set; }
     }
 
-    public class ScheduleTwinUpdateModel: ScheduleJobViewModel
+    public class ScheduleTwinUpdateModel : ScheduleJobViewModel
     {
         public List<DesiredPropetiesEditViewModel> DesiredProperties { get; set; }
 
