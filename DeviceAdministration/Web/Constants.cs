4f5432464bbbd18de82a44e73acaeeef038d52f8
Hua Zhang <huazhan@microsoft.com>
2017-02-28 12:02:44 +0800

    Add versioning machanisim for js code
    The old js code will be cached by browser which causes many problems after
    the new changes has been pushed. Append version parameter to the js path
    to let the browser refresh new js code whenever we change the constant
    version in the new release.

--- DeviceAdministration/Web/Constants.cs
+++ DeviceAdministration/Web/Constants.cs
@@ -6,6 +6,7 @@ namespace Microsoft.Azure.Devices.Applications.RemoteMonitoring.DeviceAdmin.Web
 {
     public static class Constants
     {
+        public const string JSVersion = "1.6.0.0";
         public const string CultureCookieName = "_culture";
         public const string DeviceIconTagName = "__icon__";
         public const string DeviceIconFullTagName = "tags." + DeviceIconTagName;
